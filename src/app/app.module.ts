ddd971ba4b77aa6a077d875dcd7daee7db808474
Felix Villamizar <felixvilla51@gmail.com>
2023-02-24 20:21:13 -0500

    Cuarto Commit Proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmpleadoComponent } from './component/empleado/empleado.component';
+import { EmpleadosComponent } from './component/Empleados/Empleados.component';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    EmpleadosComponent,
     EmpleadoComponent,
 
   ],
--- src/app/component/Empleados/Empleados.component.ts
+++ src/app/component/Empleados/Empleados.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-Empleados',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmpleadosComponent implements OnInit {
 
+
+
   constructor() { }
 
   ngOnInit() {
   }
 
+
+    
 }
