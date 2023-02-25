ff7f59b997c09cb7f8f1622b545717268de65526
Felix Villamizar <felixvilla51@gmail.com>
2023-02-25 14:16:54 -0500

    Quinto Commit Proyecto

--- src/app/component/empleado/empleado.component.ts
+++ src/app/component/empleado/empleado.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
   styleUrls: ['./empleado.component.css']
 })
-export class EmpleadoComponent {
+export class EmpleadoComponent implements OnInit {
+
+ //Nombre     = ' Felix';
+ 
+ //Apellido   =  ' Villamizar';
+
+ //Edad       =  1;
+
+ //Empresa    =  ' ThomasGreg';
+
+
+
+llamarEmpresa(value:string){
+
+}
+
+
+llamarNombre(value:string){
+
+}
+
+
+llamarApellido(value:string){
+
+}
+
+ constructor(){
+  
+ }
+
+ llamarEdad(value:string){
+
+ }
+
+
+ ngOnInit(): void {
+   
+ }
 
 }
